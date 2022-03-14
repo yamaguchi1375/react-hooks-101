80486de4b2652fae5628a63ab7451c84cfcbece7
slk.ty.yamaguchi <slk.ty.yamaguchi@gmail.com>
2022-03-14 12:00:23 +0900

    my first React Hook with useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+
+  const increment2 = () => setCount(previousCount => previousCount+1)
+  const decrement2 = () => setCount(previousCount => previousCount-1)
+
+  const reset = () => setCount(0)
+  const reset2 = () => setCount(previousCount => 0)
+
+  const double = () => setCount(count*2)
+  const double2 = () => setCount(previousCount => previousCount*2)
+
+  const devide = () => setCount(previousCount => {
+    return previousCount % 3 === 0 ? previousCount / 3 : previousCount;
+  })
+
   return (
-    <div>
-      This is a template for React app
-    </div>
-  );
+    <>
+        <div>count: {count}</div>
+        <div>
+            <button onClick={increment}>+1</button>
+            <button onClick={decrement}>-1</button>
+        </div>
+        <div>
+            <button onClick={increment2}>+1</button>
+            <button onClick={decrement2}>-1</button>
+        </div>
+        <div>
+            <button onClick={reset}>Reset</button>
+            <button onClick={reset2}>Reset</button>
+        </div>
+        <div>
+            <button onClick={double}>*2</button>
+            <button onClick={double2}>*2</button>
+        </div>
+        <div>
+            <button onClick={devide}>3の場合数だけ割った数を出す</button>
+        </div>
+    </>
+  )
 }
 
 export default App

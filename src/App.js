de7bce3b8b74c7ed6d216ab66863bd9ed74d6ad5
slk.ty.yamaguchi <slk.ty.yamaguchi@gmail.com>
2022-04-15 01:31:25 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 const App = props => {
+
   const [name, setName] = useState(props.name)
   const [price, setPrice] = useState(props.price)
 
@@ -14,8 +15,8 @@ const App = props => {
       <p>現在の{name}は、{price}円です。</p>
       <button onClick={() => setPrice(price + 1)}>+1</button>
       <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>reset</button>
-      <input value={name} onChange={e => setName(e.target.value)}></input>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
     </>
   )
 }
